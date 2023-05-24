0386b0d086e6af8134f82f90b9ac5c24b225f886
ali1924 <mohammadali229987@gmail.com>
2023-05-24 20:23:06 +0600

    react paralax used in Cover component

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {Helmet } from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import menuImg from '../../../assets/menu/menu-bg.jpg';
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <p>This is menu page</p>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
